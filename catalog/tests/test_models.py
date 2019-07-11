826356ad740d718a6096871c2c9542b007bd7f26
Kevin Kithinji <kevin.kithinji@healthcloud.co.ke>
2019-07-11 15:53:14 +0300

    pushing coverage

--- catalog/tests/test_models.py
+++ catalog/tests/test_models.py
@@ -3,18 +3,22 @@ from django.test import TestCase
 # Create your tests here.
 
 from catalog.models import Author
+from catalog.models import Book
+from catalog.models import Language
 
 
 class AuthorModelTest(TestCase):
-
+    # Setup test data, then test author labels.
     @classmethod
     def setUpTestData(cls):
-        """Set up non-modified objects used by all test methods."""
-        Author.objects.create(first_name='Big', last_name='Bob')
+        Author.objects.create(first_name='Kevin', last_name='Kithinji')
 
     def test_first_name_label(self):
+        # Get the author object to test
         author = Author.objects.get(id=1)
+        # Get the metadata for the required field and use it to query the required field data
         field_label = author._meta.get_field('first_name').verbose_name
+        # Compare the value to the expected result
         self.assertEqual(field_label, 'first name')
 
     def test_last_name_label(self):
@@ -52,3 +56,35 @@ class AuthorModelTest(TestCase):
         author = Author.objects.get(id=1)
         # This will also fail if the urlconf is not defined.
         self.assertEqual(author.get_absolute_url(), '/catalog/author/1')
+
+
+
+class BookModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Book.objects.create(title='TestyBook', isbn='872727727')
+
+    def test_book_name_label(self):
+        title = Book.objects.get(id=1)
+        field_label = title._meta.get_field('title').verbose_name
+        self.assertEqual(field_label, 'title')
+
+    def test_get_absolute_url(self):
+        book = Book.objects.get(id=1)
+        self.assertEqual(book.get_absolute_url(), '/catalog/book/1')
+
+    def test_isbn_label(self):
+        isbn = Book.objects.get(id=1)
+        field_label = isbn._meta.get_field('isbn').verbose_name
+        self.assertEqual(field_label,'ISBN')
+
+
+class LanguageModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Language.objects.create(name = 'Klingon')
+
+    def test_language_label(self):
+        name = Language.objects.get(id=1)
+        field_label = name._meta.get_field('name').verbose_name
+        self.assertEqual(field_label,'name')
