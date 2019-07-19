aa3d63e8fc758a887badd554e51f97b604cb3770
Kevin Kithinji <kevin.kithinji@healthcloud.co.ke>
2019-07-19 17:33:03 +0300

    added docs and notes.txt

--- catalog/apps.py
+++ catalog/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
 
-# include any application configuration for the app. Using this, you can configure 
+# include any application configuration for the app.
+# Using this, you can configure
 # some of the attributes of the application.
 
-# Application configuration objects store metadata for an application. Some attributes 
+# Application configuration objects store metadata for an application.
+# Some attributes
 # can be configured in AppConfig subclasses.
 
 
