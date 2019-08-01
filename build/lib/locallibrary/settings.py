3d2f4e9cb0938d73054c6e74812d82c4d8dd1782
kevin <kithinjimkevin@gmail.com>
2019-08-01 18:27:35 +0300

    config ansible

--- build/lib/locallibrary/settings.py
+++ build/lib/locallibrary/settings.py
@@ -239,6 +239,6 @@ REST_FRAMEWORK = {
 
 # The value conn_max_age=500 makes the connection persistent
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
